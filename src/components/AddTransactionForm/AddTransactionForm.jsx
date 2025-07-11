0ed7f6cb495c9e8691fdea91f72ec8d2cc85423a
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-07-11 04:02:16 +0300

    feat: Enhance AddTransactionForm with improved state management and styling

--- src/components/AddTransactionForm/AddTransactionForm.jsx
+++ src/components/AddTransactionForm/AddTransactionForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import * as yup from "yup";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -24,43 +24,62 @@ const schema = yup.object().shape({
 
 const AddTransactionForm = () => {
   const [income, setIncome] = useState(false);
+
   return (
-    <Formik validationSchema={schema} onSubmit={() => {}} initialValues={{}}>
+    <Formik
+      validationSchema={schema}
+      onSubmit={() => {}}
+      initialValues={{
+        type: income ? "income" : "expense",
+        amount: "",
+        date: new Date(),
+        comment: "",
+      }}
+    >
       <Form className={css.TransactionForm}>
         <div className={css.FormRow}>
-          <h3>Add Transaction</h3>
+          <h3 className={css.FormTitle}>Add Transaction</h3>
         </div>
         <div className={css.FormRow}>
-          <label htmlFor="incomeTrans" className={css.switchLabel}>
+          <label
+            htmlFor="incomeTrans"
+            className={
+              income === true
+                ? [css.switchLabel, css.incomeLabel]
+                : css.switchLabel
+            }
+          >
             Income
           </label>
           <div className={css.switchBox}>
-            <div className={css.incomeSwitch}>
-              <FaPlus className={css.icon} />
-            </div>
-            <Field
-              type="radio"
-              id="incomeTrans"
-              name="type"
-              value="income"
-              className={css.switchInput}
-            />
-            <div className={css.expenceSwitch}>
-              <FaMinus className={css.icon} />
-            </div>
-            <Field
-              type="radio"
-              id="expenceTrans"
-              name="type"
-              value="expence"
-              className={css.switchInput}
-            />
+            {income === true ? (
+              <div
+                onClick={() => setIncome(false)}
+                className={[css.incomeSwitch, css.switchIcon].join(" ")}
+              >
+                <FaPlus className={css.icon} />
+              </div>
+            ) : (
+              <div
+                onClick={() => setIncome(true)}
+                className={[css.expenceSwitch, css.switchIcon].join(" ")}
+              >
+                <FaMinus className={css.icon} />
+              </div>
+            )}
           </div>
-          <label htmlFor="expenceTrans" className={css.switchLabel}>
+          <label
+            htmlFor="expenceTrans"
+            className={
+              income === false
+                ? [css.switchLabel, css.expenceLabel]
+                : css.switchLabel
+            }
+          >
             Expence
           </label>
         </div>
-        <div className={[css.FormRow, css.FormRowMobile].join(" ")}>
+        <div className={css.FormRow}>
           <Field
             type="number"
             name="amount"
@@ -74,8 +93,24 @@ const AddTransactionForm = () => {
             placeholder={"07.07.2023"}
           />
         </div>
-        <div className={css.FormRow}></div>
-        <div className={css.FormRow}></div>
+        <div className={css.FormRow}>
+          <textarea
+            name="comment"
+            className={[css.FormInput, css.FormInputText].join(" ")}
+            placeholder="Comment"
+            rows={4}
+          ></textarea>
+        </div>
+        <div className={css.FormRow}>
+          <button type="submit" className={[css.FormButton, css.submitButton].join(" ")}>
+            ADD
+          </button>
+        </div>
+        <div className={css.FormRow}>
+          <button type="submit" className={css.FormButton}>
+            CANCEL
+          </button>
+        </div>
       </Form>
     </Formik>
   );
